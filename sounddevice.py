2d8a3483000e13db4757be5817c2c35d2c012ff1
tpeat <65079595+tpeat@users.noreply.github.com>
2021-02-26 14:52:39 -0500

    this one does frequency, but not sure how well

--- sounddevice.py
+++ sounddevice.py
@@ -10,9 +10,15 @@ stream = p.open(format= pyaudio.paInt16, channels = 1, rate = RATE, input = True
 
 for i in range(int(10*44100/1024)):
   data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
-  peak = np.average(np.abs(data))*2
-  bars = "#"*int(50*peak/2**16)
-  print("%04d %05d %s"%(i,peak,bars))
+  data = data * np.hanning(len(data)) #smooth the FFT by windowing data
+  fft = abs(np.fft.fft(data).real)
+  fft = fft[:len(fft)/2] #keep only the first half
+  freq = np.fft.fftfreq(CHUNK, 1.0/RATE)
+  freqPeak = freq[np.where(fft==np.max(fft))[0][0]]+1
+  print("peak frequency: %d Hz"%freqPeak)
+  #eak = np.average(np.abs(data))*2
+  #bars = "#"*int(50*peak/2**16)
+  #print("%04d %05d %s"%(i,peak,bars))
 
 stream.stop_stream()
 stream.close()
