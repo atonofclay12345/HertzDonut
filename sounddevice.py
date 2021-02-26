1a6d573c352b23e73e9c5469e18ec270e95b441f
tpeat <65079595+tpeat@users.noreply.github.com>
2021-02-26 18:00:07 -0500

    idk

--- sounddevice.py
+++ sounddevice.py
@@ -2,39 +2,37 @@ import pyaudio
 import numpy as np
 import aubio
 
-CHUNK = 2**11
-RATE = 44100
 
-p = pyaudio.PyAudio()
-stream = p.open(format= pyaudio.paInt16, channels = 1, rate = RATE, input = True, frames_per_buffer = CHUNK)
+class Sound:
+
+  CHUNK = 2**11
+  RATE = 44100
+
+  p = pyaudio.PyAudio()
+  stream = p.open(format= pyaudio.paInt16, channels = 1, rate = RATE, input = True, frames_per_buffer = CHUNK)
 
 #initiating aubio's pitch detection object
-pDetection = aubio.pitch("default", CHUNK, (CHUNK//2), RATE)
+  Detection = aubio.pitch("default", CHUNK, (CHUNK//2), RATE)
 
-
-for i in range(int(10*44100/1024)):
-  data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
-  data = data * np.hanning(len(data)) #smooth the FFT by windowing data
-  fft = abs(np.fft.fft(data).real)
-  fft = fft[:len(fft)/2] #keep only the first half
-  freq = np.fft.fftfreq(CHUNK, 1.0/RATE)
-  freqPeak = freq[np.where(fft==np.max(fft))[0][0]]+1
-  print("peak frequency: %d Hz"%freqPeak)
+  for i in range(int(10*44100/1024)):
+    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
+    data = data * np.hanning(len(data)) #smooth the FFT by windowing data
+    fft = abs(np.fft.fft(data).real)
+    fft = fft[:len(fft)/2] #keep only the first half
+    freq = np.fft.fftfreq(CHUNK, 1.0/RATE)
+    freqPeak = freq[np.where(fft==np.max(fft))[0][0]]+1
+    print("peak frequency: %d Hz"%freqPeak)
   #eak = np.average(np.abs(data))*2
   #bars = "#"*int(50*peak/2**16)
   #print("%04d %05d %s"%(i,peak,bars))
   
-  volume = np.sum(data**2)/len(data)
-  print("volume: %d"%volume)
-stream.stop_stream()
-stream.close()
-p.terminate()
+    volume = np.sum(data**2)/len(data)
+    print("volume: %d"%volume)
+  stream.stop_stream()
+  stream.close()
+  p.terminate()
 
-#import sounddevice as sd
-
-#for i in range(30): #30 updates in 1 second
-  #rec = sd.rec(44100/30)
-  #sounddevice.wait()
-  #print(rec.shape)
-
+if '__name__' == '__main__':
+  s = Sound()
+  
 
